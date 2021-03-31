507b21a25a8a7ab0529f0f50485a3d81b7a25555
wwkorablin <wwkorablin@gmail.com>
2021-03-31 14:21:08 +0300

    Start Calculator

---
+++ js/umnojenie.js
@@ -0,0 +1,7 @@
+var firstNum = prompt ("Введите первое число");
+var secondNum = prompt ("Введите второе число");
+var resultat = firstNum * secondNum;
+
+
+document.writeln ("Итого: ", resultat);
+
---
+++ nulled.js
@@ -0,0 +1,6 @@
+var firstNum = prompt ("Введите первое число");
+var secondNum = prompt ("Введите второе число");
+var resultat = +firstNum + +secondNum;
+
+
+document.writeln ("Итого: ", resultat);
